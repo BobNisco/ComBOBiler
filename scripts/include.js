1144f3d92fa71cdadb76ebb9b5b11d197bb5c32a
Bob Nisco <BobNisco@gmail.com>
2014-01-24 15:19:37 -0500

    Basic set up with TS/JS

---
+++ scripts/include.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var compileButton = $('#compile-button');
+    var taSourceCode = $('#taSourceCode');
+    var taOutput = $('#taOutput');
+
+    compileButton.on('click', function (e) {
+        e.preventDefault();
+    });
+});
